e91ecc0bfeaec8b95011598ef56897b8f46f5026
Leonardo Reis <leonardo.reis@vtex.com>
2024-10-21 10:38:13 -0300

    Transfer output location to videos folder

--- video_editor/__main__.py
+++ video_editor/__main__.py
@@ -9,27 +9,6 @@ from features.preprocess_videos import PreprocessVideos
 from features.remove_silence import RemoveSilence
 from entities.timeline import Timeline
 
-def create_fcpxml():
-    """
-    Create the FCPXML tree element.
-    """
-    return etree.Element('fcpxml', version="1.11")
-
-
-def add_default_configuration(file):
-    """
-    Add default configuration to the FCPXML file in the correct order.
-    """
-    file.write(b'<?xml version="1.0" encoding="UTF-8"?>\n')
-    file.write(b'<!DOCTYPE fcpxml>\n')
-
-
-def create_fcpxml_tree(fcpxml):
-    """
-    Create the FCPXML tree element.
-    """
-    return etree.ElementTree(fcpxml)
-
 
 def main():
     print("Creating FCPXML file...")
@@ -41,16 +20,13 @@ def main():
     # Parse the arguments
     args = parser.parse_args()
 
-    # Create the FCPXML Element object
-    fcpxml = create_fcpxml()
-
-    # Create the Timeline object
-    timeline = Timeline(fcpxml)
-
     # Preprocess videos
     preprocess = PreprocessVideos(args.videos_folder)
     if not args.skip_preprocess:
         preprocess.preprocess_all_videos_in_folder()
+
+    # Create the Timeline object
+    timeline = Timeline(preprocess.preprocessed_folder)
 
     # Concatenate files
     concatenate = Concatenate(timeline, preprocess.preprocessed_folder)
@@ -60,24 +36,11 @@ def main():
     remove_silence = RemoveSilence(timeline, preprocess.preprocessed_folder)
     remove_silence.remove_silence_from_videos()
 
-
     # Add subtitles
 
 
-    # Insert fcpxml Element in the Tree object
-    tree = create_fcpxml_tree(fcpxml)
-
-    with open('my_project.fcpxml', 'wb') as file:
-        # Write default configuration in file
-        add_default_configuration(file)
-
-        # Indent the tree to 4 spaces
-        etree.indent(tree, space='    ')
-
-        # Add FCPXML tree to the file
-        tree.write(file, encoding='UTF-8', pretty_print=True)
-
-    print("FCPXML file created successfully.")
+    # Create the FCPXML file
+    timeline.generate_fcpxml_file()
 
 
 if __name__ == "__main__":
--- video_editor/entities/timeline.py
+++ video_editor/entities/timeline.py
@@ -1,11 +1,44 @@
 """
 This entity represents the final timeline.
 """
+from lxml import etree
+import os
 
 
 class Timeline():
-    def __init__(self, fcpxml):
-        self.fcpxml = fcpxml
+    def __init__(self, videos_folder):
+        self.fcpxml = etree.Element('fcpxml', version="1.11")
+        self.tree = etree.ElementTree(self.fcpxml)
         self.spine = None
         self.video_assets_refs = []
         self.video_assets = {}
+
+        self.output_folder = os.path.join(videos_folder, 'timeline')
+        self.fcpxml_filename = os.path.join(self.output_folder, 'timeline.fcpxml')
+
+
+    def add_default_header(self, file):
+        """
+        Add the default header to the FCPXML file.
+        """
+        file.write(b'<?xml version="1.0" encoding="UTF-8"?>\n')
+        file.write(b'<!DOCTYPE fcpxml>\n')
+
+
+    def generate_fcpxml_file(self):
+        """
+        Create the FCPXML file.
+        """
+        os.makedirs(self.output_folder, exist_ok=True)
+
+        with open(self.fcpxml_filename, 'wb') as file:
+            # Write default configuration in file
+            self.add_default_header(file)
+
+            # Indent the tree to 4 spaces
+            etree.indent(self.tree, space='    ')
+
+            # Add FCPXML tree to the file
+            self.tree.write(file, encoding='UTF-8', pretty_print=True)
+
+        print("FCPXML file created successfully.")
